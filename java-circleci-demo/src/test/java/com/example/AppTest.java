95626ffd42c20004a7554375c982f6cbda725e90
sharifzadeh <amirsharifzadeh@gmail.com>
2023-10-25 16:31:34 -0400

    dsdsd

---
+++ java-circleci-demo/src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MyAppTest {
+    @Test
+    public void testApp() {
+        assertEquals(4, 2 + 2);
+    }
+}
