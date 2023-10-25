2cb56a0b5581535451213934afc10d62ae7b866e
Amir Sharifzadeh <2089846+sharifzadeh@users.noreply.github.com>
2023-10-25 19:45:19 -0400

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -6,6 +6,6 @@ import static org.junit.Assert.assertEquals;
 public class AppTest {
     @Test
     public void testApp() {
-        assertEquals(41, 2 + 2);
+        assertEquals(4, 2 + 2);
     }
 }
